01c7479945619631242e65bfc43caa7ea9370002
Centhor <vincenthizon266@gmail.com>
2025-11-25 10:11:51 +0800

    Base frontend cartpage

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { BookList } from './landing-page/landing-page';
 import { Login } from './login/login';
 import { GenrePage} from './genre-page/genre-page';
+import { CartPage } from './cart-page/cart-page';
 
 export const routes: Routes = [
     { path: '', component: BookList, pathMatch: 'full'},
     { path: 'login', component: Login, pathMatch: 'full'},
-    { path: 'genre', component: GenrePage, pathMatch: 'full'}
+    { path: 'genre', component: GenrePage, pathMatch: 'full'},
+    { path: 'cart', component: CartPage, pathMatch: 'full'}
 ];
---
+++ frontend/src/app/order-api.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Book } from './book-api';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+interface OrderItem{
+  book : Book,
+  quantity : number
+}
+
+export interface Order{
+  id: number,
+  userId: number,
+  book
+  totalCost: number,
+  status: string,
+  isFulfilled: boolean,
+  orderDate: Date,
+  fulfilledDate: Date,
+    // private Integer id;
+    // private Integer userId;
+    // private Map<Book, Integer> books; //value = quantity
+    // private Double totalCost;
+    // private String status;
+    // private Boolean isFulfilled;
+    // private LocalDateTime orderDate;
+    // private LocalDateTime fulfilledDate;
+
+}
+
+export class OrderApi {
+  
+}
